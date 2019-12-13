bcd8be05fb3731363954220facfe2ba21aa9a764
Yahia <algerinooh@outlook.fr>
2019-12-13 16:13:28 +0100

    ça marche

--- client/js/app.js
+++ client/js/app.js
@@ -2,29 +2,45 @@ $.getJSON("http://127.0.0.1:8000/api/ponts", function (datas) {
     let lesPonts = JSON.parse(JSON.stringify(datas));
     lesPonts.sort((a, b) => (a.commune > b.commune) ? 1 : ((b.commune > a.commune) ? -1 : 0)); //ranger les ponts par ordre alphabetique commune
     lesPonts.forEach(unPont => {
-        const typePont = unPont.elem_patri.split(' ')[0];
+        function pontViaducOuPasserelle() {
+            let typePont = unPont.elem_patri.split(' ')[0].toLowerCase();
+            return unPont.elem_patri.split(' ')[0].toLowerCase() === 'pont' ? 'pont' : typePont === 'passerelle' ? 'passerelle' : typePont === 'viaduc' ? 'viaduc' : typePont === 'front' ? 'front' : 'autre';
+            /*            if (typePont === 'pont') {
+                            typePont = 'pont';
+                        } else if (typePont === 'passerelle') {
+                            typePont = 'passerelle';
+                        } else if (typePont === 'viaduc') {
+                            typePont = 'viaduc';
+                        } else {
+                            typePont = 'autres';
+                        }*/
+        }
+
         $('.listePonts').append(`
                 <tr>
                     <td class="commune">${unPont.commune}</td>
                     <td>${unPont.elem_patri}</td>
                     <td style="text-align: center">${unPont.identifiant.substring(0, 2)}</td>
-                    <td >${unPont.elem_princ}</td>
+                    <td>${unPont.elem_princ}</td>
+                    <td class="typePont" hidden> ${pontViaducOuPasserelle(unPont.elem_patri.split(' ')[0])}</td>
                 </tr>
         `);
     });
 });
 
-function filterById() { //recupere l'attribut value de la checkbox qui à été cliquée
-    $('input:checkbox').click(function () {
+
+$(document).ready(function () {
+    $("input:checkbox").on("change", function () {
+        var a = $("input:checkbox:checked").val();
+        console.log(a);
         if ($(this).prop("checked")) {
-            $(".commune:not(:contains(" + $(this).attr('value') + "))").parent().parent().parent().hide();
+            $(".typePont:not(:contains(" + a + "))").parent().hide();
         } else {
-            $(".commune:not(:contains(" + $(this).attr('value') + "))").parent().parent().parent().show();
+            $(".typePont").parent().show();
         }
     });
-}
+});
 
-filterById();
 
 //stocker les favoris localement comme dans le TP avec storage.js
 function addFavoris(id) {
