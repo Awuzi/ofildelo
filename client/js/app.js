5b240874b59aa470fbc010ab69062be48b967a40
Yahia <algerinooh@outlook.fr>
2019-12-14 16:04:37 +0100

    fonction de filtrage foncitonnent séparément

--- client/js/app.js
+++ client/js/app.js
@@ -1,12 +1,9 @@
 $.getJSON("http://127.0.0.1:8000/api/ponts", function (datas) {
-    let lesPonts = JSON.parse(JSON.stringify(datas));
-    lesPonts.sort((a, b) => (a.commune > b.commune) ? 1 : ((b.commune > a.commune) ? -1 : 0)); //ranger les ponts par ordre alphabetique commune
+    let lesPonts = JSON.parse(JSON.stringify(datas)); //add to array
+    //ranger les ponts par ordre alphabetique commune
+    lesPonts.sort((a, b) => (a.commune > b.commune) ? 1 : ((b.commune > a.commune) ? -1 : 0));
     lesPonts.forEach(unPont => {
-        let pontty = () => {
-
-        };
-        let pontViaducOuPasserelle = () => {
-            let typePont = unPont.elem_patri.split(' ')[0].toLowerCase();
+        let pontViaducOuPasserelle = (typePont) => {
             return typePont === 'pont' ? 'pont' : typePont === 'passerelle' ? 'passerelle' : typePont === 'viaduc' ? 'viaduc' : typePont === 'front' ? 'front' : 'autre';
             /*
             if (typePont === 'pont') {
@@ -20,10 +17,8 @@ $.getJSON("http://127.0.0.1:8000/api/ponts", function (datas) {
             }
             */
         };
-
-        let getDepartement = () => {
-            let dep = unPont.identifiant.substring(0, 2);
-            return dep === '77' ? '77' : dep === '78' ? '78' : dep === '91' ? '91' : dep === '92' ? '92' : dep === '93' ? '93' : dep === '94' ? '94' : dep === '95' ? '95' : null;
+        let getDepartement = (dep) => {
+            return dep === '75' ? '75' : dep === '77' ? '77' : dep === '78' ? '78' : dep === '91' ? '91' : dep === '92' ? '92' : dep === '93' ? '93' : dep === '94' ? '94' : dep === '95' ? '95' : null;
         };
         $('.listePonts').append(`
             <tr>
@@ -31,13 +26,13 @@ $.getJSON("http://127.0.0.1:8000/api/ponts", function (datas) {
                 <td>${unPont.elem_patri}</td>
                 <td class="departement" style="text-align: center">${getDepartement(unPont.identifiant.substring(0, 2))}</td>
                 <td>${unPont.elem_princ}</td>
-                <td class="typePont" hidden> ${pontViaducOuPasserelle(unPont.elem_patri.split(' ')[0])}</td>
+                <td class="typePont" hidden> ${pontViaducOuPasserelle(unPont.elem_patri.split(' ')[0].toLowerCase())}</td>
             </tr>
         `);
     });
 });
 
-
+//filtre type
 $(document).ready(function () {
     $("input:checkbox").on("change", function () {
         const a = $("input:checkbox:checked").val();
@@ -50,7 +45,7 @@ $(document).ready(function () {
     });
 });
 
-
+//filtre departements
 $(document).ready(function () {
     $("input:checkbox").on("change", function () {
         const a = $("input:checkbox:checked").val();
